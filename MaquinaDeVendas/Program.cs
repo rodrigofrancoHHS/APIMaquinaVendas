6f411e8aab1fdb33e4e4a39ef775b0d4e96df1d8
rodrigofrancoHHS <133037096+rodrigofrancoHHS@users.noreply.github.com>
2023-05-31 15:28:38 +0100

    HHS

--- MaquinaDeVendas/Program.cs
+++ MaquinaDeVendas/Program.cs
@@ -4,13 +4,23 @@ using MaquinaDeVendas.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ProdutosContext>(opt =>
     opt.UseInMemoryDatabase("ProdutosList"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -24,6 +34,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(); // Enable CORS
 
 app.MapControllers();
 
