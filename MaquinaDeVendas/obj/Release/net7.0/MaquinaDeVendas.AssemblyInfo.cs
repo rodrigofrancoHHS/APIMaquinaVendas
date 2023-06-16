a53cec057e676b72236e34a70ee0dc9690a011b9
LeonardoBernard0 <136629452+LeonardoBernard0@users.noreply.github.com>
2023-06-16 10:40:56 +0100

    Atualização do branch

--- MaquinaDeVendas/obj/Release/net7.0/MaquinaDeVendas.AssemblyInfo.cs
+++ MaquinaDeVendas/obj/Release/net7.0/MaquinaDeVendas.AssemblyInfo.cs
@@ -11,8 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.OpenApi")]
-[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("MaquinaDeVendas")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("MaquinaDeVendas")]
+[assembly: System.Reflection.AssemblyTitleAttribute("MaquinaDeVendas")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
 
